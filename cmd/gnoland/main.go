4b372b42fc9222ecb9ca3b3219777c5a9a7662f1
Giancarlos Salas <me@giansalex.dev>
2022-09-30 11:50:48 -0500

    Add home dir flag

--- cmd/gnoland/main.go
+++ cmd/gnoland/main.go
@@ -38,6 +38,7 @@ var flags struct {
 	genesisBalancesFile   string
 	genesisTxsFile        string
 	chainID               string
+	homeDir               string
 	genesisRemote         string
 }
 
@@ -48,11 +49,12 @@ func runMain(args []string) error {
 	fs.StringVar(&flags.genesisBalancesFile, "genesis-balances-file", "./gnoland/genesis/genesis_balances.txt", "initial distribution file")
 	fs.StringVar(&flags.genesisTxsFile, "genesis-txs-file", "./gnoland/genesis/genesis_txs.txt", "initial txs to replay")
 	fs.StringVar(&flags.chainID, "chainid", "dev", "chainid")
+	fs.StringVar(&flags.homeDir, "home", "testdir", "Home dir")
 	fs.StringVar(&flags.genesisRemote, "genesis-remote", "localhost:26657", "replacement for '%%REMOTE%%' in genesis")
 	fs.Parse(args)
 
 	logger := log.NewTMLogger(log.NewSyncWriter(os.Stdout))
-	rootDir := "testdir"
+	rootDir := flags.homeDir
 	cfg := config.LoadOrMakeConfigWithOptions(rootDir, func(cfg *config.Config) {
 		cfg.Consensus.CreateEmptyBlocks = false
 		cfg.Consensus.CreateEmptyBlocksInterval = 60 * time.Second
